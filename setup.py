e6e96d9fa725ec28028b090c900086474e69cdb8
Mike Boers <github@mikeboers.com>
2010-05-25 17:15:33 -0400

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+
+setup(
+    name='litemap',
+    version='1.0a',
+    description='Mapping class which stores in SQLite database.',
+    url='http://github.com/mikeboers/LiteMap',
+    py_modules=['litemap'],
+    
+    author='Mike Boers',
+    author_email='litemap@mikeboers.com',
+    license='New BSD License',
+)
