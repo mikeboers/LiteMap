f75de3403b9d93591531dfd3bda1957411fcc3e1
Mike Boers <github@mikeboers.com>
2011-04-05 10:09:14 -0400

    Add classifiers, remove "alpha" status.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 **LiteMap** is a Python class which behaves like a dictionary, but persists to
-a SQLite database.
+a table within a SQLite database.
 
 The `LiteMap` will only map strings to other strings. If you would like to use more complex values then wrap this object in a `shelve.Shelf`. If you would like to use more complex keys and values then wrap this object in a `SerialView`.
 
--- setup.py
+++ setup.py
@@ -3,12 +3,23 @@ from distutils.core import setup
 
 setup(
     name='LiteMap',
-    version='0.2.1a',
+    version='0.2.1',
     description='Mapping class which stores in SQLite database.',
     url='http://github.com/mikeboers/LiteMap',
     py_modules=['litemap'],
     
     author='Mike Boers',
     author_email='litemap@mikeboers.com',
-    license='New BSD License',
+    license='BSD-3',
+    
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Database',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
